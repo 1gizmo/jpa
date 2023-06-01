c02536b5b6998107883368d4bacdd21b53b94e0d
1gizmo <126743255+1gizmo@users.noreply.github.com>
2023-06-01 11:41:02 +0900

    # 종합연습문제 3

--- src/main/java/com/study/jpa/chap05_practice/service/PostService.java
+++ src/main/java/com/study/jpa/chap05_practice/service/PostService.java
@@ -1,9 +1,7 @@
 package com.study.jpa.chap05_practice.service;
 
-import com.study.jpa.chap05_practice.dto.PageDTO;
-import com.study.jpa.chap05_practice.dto.PageResponseDTO;
-import com.study.jpa.chap05_practice.dto.PostDetailResponseDTO;
-import com.study.jpa.chap05_practice.dto.PostListResponseDTO;
+import com.study.jpa.chap05_practice.dto.*;
+import com.study.jpa.chap05_practice.entity.HashTag;
 import com.study.jpa.chap05_practice.entity.Post;
 import com.study.jpa.chap05_practice.repository.HashTagRepository;
 import com.study.jpa.chap05_practice.repository.PostRepository;
@@ -25,17 +23,17 @@ import java.util.stream.Collectors;
 @Transactional // JPA 레파지토리를 사용하는 빈은 반드시 트랜잭션 아노테이션 필수
 public class PostService {
 
-            private final PostRepository postRepository;
-            private final HashTagRepository hashTagRepository;
+    private final PostRepository postRepository;
+    private final HashTagRepository hashTagRepository;
 
     public PostListResponseDTO getPosts(PageDTO dto) {
 
         // Pageable 객체 생성
         Pageable pageable = PageRequest.of(
-                dto.getPage()-1,
+                dto.getPage() - 1,
                 dto.getSize(),
                 Sort.by("createDate").descending()
-                );
+        );
 
         // 데이터베이스에서 게시물 목록 조회
         Page<Post> posts = postRepository.findAll(pageable);
@@ -59,4 +57,42 @@ public class PostService {
                 .build();
 
     }
+
+    public PostDetailResponseDTO getDetail(Long id) {
+
+        Post postEntity = postRepository.findById(id)
+                .orElseThrow(
+                        () -> new RuntimeException(
+                                id + "번 게시물이 존재하지 않습니다 ! "
+                        )
+                );
+        // postEntity 를 dto로 변환해서 리턴
+        return new PostDetailResponseDTO(postEntity);
+    }
+
+    public PostDetailResponseDTO insert(final PostCreateDTO dto)
+            throws RuntimeException {
+
+        // 게시물 저장
+        Post saved = postRepository.save(dto.toEntity());
+
+        // 해시태그 저장
+        List<String> hashTags = dto.getHashTags();
+        if (hashTags != null && hashTags.size() > 0) {
+            hashTags.forEach(ht -> {
+                hashTagRepository.save(
+                        HashTag.builder()
+                                .tagName(ht)
+                                .post(saved)
+                                .build()
+                );
+            });
+        }
+
+        return new PostDetailResponseDTO(saved);
+
+
+    }
+
+
 }
